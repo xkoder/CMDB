19912bfe9e80b261f43a67792b0f47bb459c4e12
xkodali <srinivas.kodali@hotmail.com>
2012-01-28 02:53:08 +0530

    Initial commit' '

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaticPages::Application.config.secret_token = '1046bf34562b94b88160c88064feb1a458475cd0808b59b341dbc1ff54a6ef8cdf100ccf4a1430492813245d83be91d74423217c19b5f777a137f309edda93ee'
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe PagesController do
+render_views
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+    
+    it "should have right title" do
+      get 'home'
+      response.should have_selector("title", :content => "StaticPages | Home")
+    end  
+    it "should have non blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+    
+    it "should have right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "StaticPages | Contact")
+    end  
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+        it "should have right title" do
+      get 'about'
+      response.should have_selector("title", :content => "StaticPages | About")
+    end  
+  end
+
+end
