1727901b5aac054331f1692446ee44d4e11587c2
xkodali <srinivas.kodali@hotmail.com>
2012-02-11 02:48:56 -0600

    Modelling Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
-  def new
-  	@title = "Sign up"
-  end
 
   def show
   	@user = User.find(params[:id])
+  	@title = @user.name
+  end
+
+  def new
+  	@title = "Sign up"
   end
 
 end
