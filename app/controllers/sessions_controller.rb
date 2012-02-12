6a886e879b8ec1664ece80f1578e678bb2b9b22a
xkodali <srinivas.kodali@hotmail.com>
2012-02-11 23:35:28 -0600

    working user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
 	def new
 		@title = "Sign in"
 	end
@@ -11,7 +12,8 @@ class SessionsController < ApplicationController
 			@title = "Sign in"
 			render 'new'
 		else
-			# later
+			sign_in user
+			redirect_to user
 		end
 	end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,11 +18,19 @@ class User < ActiveRecord::Base
 
     before_save :encrypt_password
 
-    def self.authenticate(email,submitted_password)
-      user = find_by_email(email)
-      return nil if user.nil?
-      return user if user.has_password?(submitted_password)
+    class << self
+
+      def authenticate(email,submitted_password)
+        user = find_by_email(email)
+        (user && user.has_password?(submitted_password))? user : nil
+      end
+
+      def authenticate_with_salt(id, cookie_salt)
+        user = find_by_id(id)
+        (user && user.salt == cookie_salt)? user : nil
+      end
     end
+
 
     def has_password?(submitted_password)
       encrypted_password == encrypt(submitted_password)
